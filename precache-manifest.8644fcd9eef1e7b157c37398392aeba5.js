93b3267068e3d165b02a6b68f180d4adedaac693
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-08-28 13:37:08 +0900

    Updates

---
+++ precache-manifest.8644fcd9eef1e7b157c37398392aeba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b93cfbfd81c9f1b7cacc6a69f4a0343",
+    "url": "/2020-datayanolja/index.html"
+  },
+  {
+    "revision": "73887ed03d5077959d20",
+    "url": "/2020-datayanolja/static/css/main.78610cfa.chunk.css"
+  },
+  {
+    "revision": "db98e2e4ddb530c7c6f2",
+    "url": "/2020-datayanolja/static/js/2.e070f058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-datayanolja/static/js/2.e070f058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73887ed03d5077959d20",
+    "url": "/2020-datayanolja/static/js/main.7fa94bcf.chunk.js"
+  },
+  {
+    "revision": "8e583300a1fb81698e90",
+    "url": "/2020-datayanolja/static/js/runtime-main.2a32866b.js"
+  }
+]);
