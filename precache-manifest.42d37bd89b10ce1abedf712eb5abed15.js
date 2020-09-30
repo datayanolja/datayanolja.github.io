d8aa5c6fd338516b6047408a84029804be8c4bfc
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-10-01 04:24:48 +0900

    Updates

---
+++ precache-manifest.42d37bd89b10ce1abedf712eb5abed15.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437f4fbae13ca0cc15f55401224dc86f",
+    "url": "/github.io/datayanolja/index.html"
+  },
+  {
+    "revision": "508eae53710710ea4b7a",
+    "url": "/github.io/datayanolja/static/css/main.a489042f.chunk.css"
+  },
+  {
+    "revision": "a5c8f224fe52387334db",
+    "url": "/github.io/datayanolja/static/js/2.20b72fc6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/github.io/datayanolja/static/js/2.20b72fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508eae53710710ea4b7a",
+    "url": "/github.io/datayanolja/static/js/main.11062abe.chunk.js"
+  },
+  {
+    "revision": "ae06fde8b50defad5b6d",
+    "url": "/github.io/datayanolja/static/js/runtime-main.e196ebf2.js"
+  },
+  {
+    "revision": "b12bb6be0abf4512db8f5b0302187cab",
+    "url": "/github.io/datayanolja/static/media/Archieve_img.b12bb6be.png"
+  },
+  {
+    "revision": "b5a6b70f9860e3dbc7d3d892e65a0b95",
+    "url": "/github.io/datayanolja/static/media/Coc_img.b5a6b70f.png"
+  },
+  {
+    "revision": "2d94377fc203723d33f33b8d9839e977",
+    "url": "/github.io/datayanolja/static/media/Concept.2d94377f.png"
+  },
+  {
+    "revision": "73c160630984c57e4b77b819016689ed",
+    "url": "/github.io/datayanolja/static/media/GothicA1-Black.73c16063.ttf"
+  },
+  {
+    "revision": "2efc91cd70fc5b47b1249c9e7ddd64d2",
+    "url": "/github.io/datayanolja/static/media/GothicA1-Light.2efc91cd.ttf"
+  },
+  {
+    "revision": "25ab3626b77d039abe87a29ef5e78de9",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Black.25ab3626.otf"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "1b0504a3e741f8e6e07b297cb8f55d2d",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Light.1b0504a3.otf"
+  },
+  {
+    "revision": "3e344d255a9c2bc603683f5b078d471d",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Medium.3e344d25.otf"
+  },
+  {
+    "revision": "839a2f6c607e26dee10678a077729ba5",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Regular.839a2f6c.otf"
+  },
+  {
+    "revision": "3c91862c95752f24cd41785301373e7b",
+    "url": "/github.io/datayanolja/static/media/NotoSansKR-Thin.3c91862c.otf"
+  },
+  {
+    "revision": "3aede05c99ca69f18d68273e915f98cb",
+    "url": "/github.io/datayanolja/static/media/back_04.3aede05c.png"
+  },
+  {
+    "revision": "8f0dc04748e223e121345716628d936c",
+    "url": "/github.io/datayanolja/static/media/date.8f0dc047.png"
+  },
+  {
+    "revision": "bcdb01c240c43c42c3014341cae6aaf8",
+    "url": "/github.io/datayanolja/static/media/guide_02.bcdb01c2.png"
+  },
+  {
+    "revision": "7f91227553c82d35bd68a5fbf04dc459",
+    "url": "/github.io/datayanolja/static/media/guide_03.7f912275.png"
+  },
+  {
+    "revision": "19834ed8f9fefbb129ed28e81d8a00ea",
+    "url": "/github.io/datayanolja/static/media/guide_04.19834ed8.png"
+  },
+  {
+    "revision": "752d4df06f719977d11a13a08212a802",
+    "url": "/github.io/datayanolja/static/media/instagram.752d4df0.png"
+  },
+  {
+    "revision": "064f23a084a0032a7e581d37118d8d12",
+    "url": "/github.io/datayanolja/static/media/sponsor.064f23a0.png"
+  },
+  {
+    "revision": "2f9d65acb171321b02f8370597d67556",
+    "url": "/github.io/datayanolja/static/media/time_table.2f9d65ac.png"
+  },
+  {
+    "revision": "39843a130c07b8b250d86b7537f0bf86",
+    "url": "/github.io/datayanolja/static/media/title_04.39843a13.png"
+  },
+  {
+    "revision": "4d36d226745cdee4717913055384f0e3",
+    "url": "/github.io/datayanolja/static/media/이일섭.4d36d226.png"
+  }
+]);
